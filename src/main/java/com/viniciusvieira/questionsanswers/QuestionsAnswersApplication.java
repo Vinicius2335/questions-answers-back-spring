112526aaa4742a41165b93bdc9718b06d992db16
Vinicius2335 <55859404+Vinicius2335@users.noreply.github.com>
2022-07-29 16:51:42 -0300

    project init

---
+++ src/main/java/com/viniciusvieira/questionsanswers/QuestionsAnswersApplication.java
@@ -0,0 +1,13 @@
+package com.viniciusvieira.questionsanswers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuestionsAnswersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuestionsAnswersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/viniciusvieira/questionsanswers/QuestionsAnswersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.viniciusvieira.questionsanswers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuestionsAnswersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
